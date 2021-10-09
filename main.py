aba3854ab941371ff09f8f1d502d7c7773b34428
Anmol_Singh <anmolsingh9822@gmail.com>
2021-10-09 19:19:25 +0530

    Final commit

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import csv
 print("Assuming that each column refers to the same user in both test.csv and train.csv data and.......")
 print("Reading the csv files..........")
 
+FinalCSV1=["Users"]
+FinalCSV2=["Score"]
+
 with open("test.csv") as test_file:
     test_reader=csv.reader(test_file)
 
@@ -30,14 +33,41 @@ with open("test.csv") as test_file:
             tmp[i+1]=score_1
         main_test.append(list(set(tmp)))
     print(tmp)
-    # for i in tmp:
-        
 
+    print("The following customer is the most satisfied and most likely to repeate the transaction:: ")
+    values=tmp.values()
+    keys=tmp.keys()
+    maxximum=max(values)
+    tmp2=[]
+    for j in range(maxximum,-1,-1):
+        for i in tmp:
+            if tmp[i] == j:
+                tmp2.append("user: var_"+str(tmp[i]+1))   
+                FinalCSV1.append(tmp[i])
+    tmp2=set(tmp2)
+    for i in tmp2:
+        print(i)        
+    for i in range(200):
+        FinalCSV1.append("var_"+str(i))
 
+    # open the file in the write mode
+    f = open('output.csv', 'w')
 
-    # #using DATA from train.csv
-    # with open("train.csv") as train_file:
-    #     train_reader=csv.reader(train_file)
+    # create the csv writer
+    writer = csv.writer(f)
+
+    # write a row to the csv file
+    writer.writerow(FinalCSV1)
+    writer.writerow(FinalCSV2)
+
+    # close the file
+    f.close()
+
+print("Due to limitted resources Model could not train ")
+
+#sing DATA from train.csv
+with open("train.csv") as train_file:
+    train_reader=csv.reader(train_file)
 
     #     next(train_reader)
 
@@ -51,10 +81,3 @@ with open("test.csv") as test_file:
     #             tmp[i+1]=score_1
     #         main_test.append(list(set(tmp)))
     #     print(tmp)
-                
-
-
-
-
-
-        # print("The order in which there will be user retention is:  ")
